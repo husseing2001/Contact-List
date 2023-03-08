e3341088463120abc6b8f610d3787d297a148590
husse <hussein.g.abouzeid@gmail.c>
2023-03-07 21:55:36 -0500

    second commit

--- contactList/settings.py
+++ contactList/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication'
 ]
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'authentication.backends.JWTAuthentication',
+    )    
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -112,6 +121,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+#JWT
+
+
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+
+
 
 
 # Static files (CSS, JavaScript, Images)
